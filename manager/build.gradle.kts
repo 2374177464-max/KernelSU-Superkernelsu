1952da55035604bca4e10ec4c6f9206718754124
2374177464-max <2374177464@qq.com>
2026-09-08 20:08:54 +0800

    1

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -27,7 +27,7 @@ fun getGitDescribe(): String {
 
 fun getVersionCode(): Int {
     val commitCount = getGitCommitCount()
-    return 30000 - 7 + commitCount
+    return 30000 - 9 + commitCount
 }
 
 fun getVersionName(): String {
